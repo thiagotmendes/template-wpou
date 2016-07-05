05305c95f15171a0885802a7b9f7dcfe6fb0a0ef
Thiago Mendes <thiagotmendes@outlook.com>
2016-07-05 20:12:14 -0300

    ultimos ajustes pousada

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php get_header() ?>
 
     <section>
-      <?php echo do_shortcode('[rev_slider alias="slider_home"]') ?> ?>
+      <?php echo do_shortcode('[rev_slider alias="slider_home"]') ?>
     </section>
 
     <section class="apresentacao">
@@ -84,8 +84,76 @@
       </div>
     </section>
 
+    <section class="vantangens-mobile">
+      <div class="container">
+        <h3 class="titulo-site text-uppercase">Vantagens</h3>
+        <div class="row linha-vantagens">
+          <div class="col-md-6">
+            <div class="row">
+              <div class="col-md-8" >
+                <h3 class="titulo-servicos">Criaçãode sites</h3>
+                <div class="">
+                  Layouts modernos e dinâmicos. Aumente as possibilidades de negócio com um site bonito,
+                  bem apresentável que convença seus clientes que a sua pousada é a melhor opção para eles.
+                </div>
+              </div>
+              <div class="col-md-4">
+                <img src="<?php echo get_template_directory_uri() ?>/images/icon1.png" alt="" class="pull-xs-right" />
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="row">
+              <div class="col-md-4">
+                <img src="<?php echo get_template_directory_uri() ?>/images/icon2.png" alt="" class="" />
+              </div>
+              <div class="col-md-8">
+                <h3 class="titulo-servicos">layouts respnsivos</h3>
+                Seus clientes estão comprando e comunicando-se em smartphones, tablets, notebooks e desktops.
+                Seu site precisa estar preparado para se adaptar a todos os dispositivos.
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row linha-vantagens">
+          <div class="col-md-6">
+            <div class="row">
+              <div class="col-md-8">
+                <h3 class="titulo-servicos">Otimização de sites</h3>
+                Não basta apenas ter um site, ele tem que ser encontrado pelos seus clientes.
+                A Otimização de Sites BH (SEO) posiciona seu site nas primeiras posições do Google
+                 e aumenta a visibilidade da sua empresa proporcionando excelentes resultados
+              </div>
+              <div class="col-md-4">
+                <img src="<?php echo get_template_directory_uri() ?>/images/icon3.png" alt="" class="pull-xs-right" />
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="row">
+              <div class="col-md-4">
+                <img src="<?php echo get_template_directory_uri() ?>/images/icon4.png" alt=""/>
+              </div>
+              <div class="col-md-8">
+                <h3 class="titulo-servicos">Sites Gerenciaveis</h3>
+                <div class="">
+                  Gerencie você mesmo o conteúdo do seu site de forma rápida através de um painel administrativo prático, de fácil navegabilidade.
+                  Tenha um site sempre atualizado e ainda diminua os custos com a manutenção de conteúdo.
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
     <section class="solucao">
       <div class="container">
+        <div class="solucao-mobile">
+          <img src="<?php echo get_template_directory_uri() ?>/images/standard.jpg" alt="" />
+          <img src="<?php echo get_template_directory_uri() ?>/images/luxo.jpg" alt="" />
+          <img src="<?php echo get_template_directory_uri() ?>/images/personalizado.jpg" alt="" />
+        </div>
         <table class="table table-solucao">
           <thead>
             <tr>
@@ -103,58 +171,58 @@
               <td rowspan="12"> <img src="<?php echo get_template_directory_uri() ?>/images/table3.jpg" alt="" /> </td>
             </tr>
             <tr>
-              <td class="lista_solucoes">Layout Responsivo</td>
+              <td class="lista_solucoes"> Linguagem HTML,CSS </td>
               <td> <img src="<?php echo get_template_directory_uri() ?>/images/check.png" alt="" /> </td>
               <td> <img src="<?php echo get_template_directory_uri() ?>/images/check.png" alt="" /> </td>
             </tr>
             <tr>
-              <td class="lista_solucoes">Layout Responsivo</td>
+              <td class="lista_solucoes">Painel Administrativo</td>
               <td> <img src="<?php echo get_template_directory_uri() ?>/images/check.png" alt="" /> </td>
               <td> <img src="<?php echo get_template_directory_uri() ?>/images/check.png" alt="" /> </td>
             </tr>
             <tr>
-              <td class="lista_solucoes">Layout Responsivo</td>
+              <td class="lista_solucoes"> Páginas Institucionais </td>
               <td> <img src="<?php echo get_template_directory_uri() ?>/images/check.png" alt="" /> </td>
               <td> <img src="<?php echo get_template_directory_uri() ?>/images/check.png" alt="" /> </td>
             </tr>
             <tr>
-              <td class="lista_solucoes">Layout Responsivo</td>
+              <td class="lista_solucoes"> Álbum de fotos </td>
               <td> <img src="<?php echo get_template_directory_uri() ?>/images/check.png" alt="" /> </td>
               <td> <img src="<?php echo get_template_directory_uri() ?>/images/check.png" alt="" /> </td>
             </tr>
             <tr>
-              <td class="lista_solucoes">Layout Responsivo</td>
+              <td class="lista_solucoes"> Formulário de contato </td>
               <td> <img src="<?php echo get_template_directory_uri() ?>/images/check.png" alt="" /> </td>
               <td> <img src="<?php echo get_template_directory_uri() ?>/images/check.png" alt="" /> </td>
             </tr>
             <tr>
-              <td class="lista_solucoes">Layout Responsivo</td>
+              <td class="lista_solucoes"> Suporte para hospedar </td>
               <td> <img src="<?php echo get_template_directory_uri() ?>/images/check.png" alt="" /> </td>
               <td> <img src="<?php echo get_template_directory_uri() ?>/images/check.png" alt="" /> </td>
             </tr>
             <tr>
-              <td class="lista_solucoes">Layout Responsivo</td>
-              <td> <img src="<?php echo get_template_directory_uri() ?>/images/check.png" alt="" /> </td>
-              <td> <img src="<?php echo get_template_directory_uri() ?>/images/check.png" alt="" /> </td>
-            </tr>
-            <tr>
-              <td class="lista_solucoes">Layout Responsivo</td>
-              <td> <img src="<?php echo get_template_directory_uri() ?>/images/check.png" alt="" /> </td>
+              <td class="lista_solucoes"> Tradução Português/Inglês </td>
+              <td>  </td>
               <td> <img src="<?php echo get_template_directory_uri() ?>/images/check.png" alt="" /> </td>
             </tr>
             <tr>
-              <td class="lista_solucoes">Layout Responsivo</td>
-              <td> <img src="<?php echo get_template_directory_uri() ?>/images/check.png" alt="" /> </td>
-              <td> <img src="<?php echo get_template_directory_uri() ?>/images/check.png" alt="" /> </td>
-            </tr>
-            <tr>
-              <td class="lista_solucoes">Layout Responsivo</td>
-              <td> <img src="<?php echo get_template_directory_uri() ?>/images/check.png" alt="" /> </td>
+              <td class="lista_solucoes"> Área de depoimentos </td>
+              <td>  </td>
               <td> <img src="<?php echo get_template_directory_uri() ?>/images/check.png" alt="" /> </td>
             </tr>
             <tr>
-              <td class="lista_solucoes">Layout Responsivo</td>
+              <td class="lista_solucoes"> <strong> Reserva online </strong> </td>
+              <td>  </td>
               <td> <img src="<?php echo get_template_directory_uri() ?>/images/check.png" alt="" /> </td>
+            </tr>
+            <tr>
+              <td class="lista_solucoes"> <strong> Otimização do site(SEO) </strong>  </td>
+              <td>  </td>
+              <td> <img src="<?php echo get_template_directory_uri() ?>/images/check.png" alt="" /> </td>
+            </tr>
+            <tr>
+              <td class="lista_solucoes"> <strong> Suporte Técnico </strong>  </td>
+              <td>  </td>
               <td> <img src="<?php echo get_template_directory_uri() ?>/images/check.png" alt="" /> </td>
             </tr>
             <tr>
